c1e03daf499fe92d5e799db0291b97f1131c27a5
Julian R Figueroa <jfigueroa@buf.build>
2023-03-17 09:51:55 -0400

    Fix release body: too long (#16)
    Truncate table with more than 10 reference rows.

--- cmd/release/main_test.go
+++ cmd/release/main_test.go
@@ -15,23 +15,26 @@
 package main
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/bufbuild/modules/internal/modules"
 	"github.com/bufbuild/modules/private/bufpkg/bufstate"
 	modulestatev1alpha1 "github.com/bufbuild/modules/private/gen/modulestate/v1alpha1"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_calculateNewReleaseModules(t *testing.T) {
+func TestCalculateNewReleaseModules(t *testing.T) {
 	t.Parallel()
 	t.Run("NewReleases-FromNoPreviousRelease", func(t *testing.T) {
 		t.Parallel()
 		currentRelease := map[string]string{
 			"envoyproxy/envoy": "bb554f53ad8d3a2a2ae4cbd7102a3e20ae00b558",
 		}
-		got, err := calculateNewReleaseModules(filepath.Join("../../test/golden/new-release", bufstate.SyncRoot), nil, currentRelease)
+		got, err := calculateNewReleaseModules(filepath.Join("testdata/golden/new-release", bufstate.SyncRoot), nil, currentRelease)
 		require.NoError(t, err)
 		require.Equal(t, map[string]releaseModuleState{
 			"envoyproxy/envoy": {
@@ -51,7 +54,7 @@ func Test_calculateNewReleaseModules(t *testing.T) {
 			"envoyproxy/envoy": "7850b6bb6494e3bfc093b1aff20282ab30b67940", // updated
 
 		}
-		got, err := calculateNewReleaseModules(filepath.Join("../../test/golden/updated-release", bufstate.SyncRoot), prevRelease, currentRelease)
+		got, err := calculateNewReleaseModules(filepath.Join("testdata/golden/updated-release", bufstate.SyncRoot), prevRelease, currentRelease)
 		require.NoError(t, err)
 		require.Equal(t, map[string]releaseModuleState{
 			"envoyproxy/envoy": {
@@ -90,7 +93,7 @@ func Test_calculateNewReleaseModules(t *testing.T) {
 			"envoyproxy/protoc-gen-validate": "38260ee45796b420276ac925d826ecec8fc3e9a8", // unchanged
 			"gogo/protobuf":                  "8892e00f944642b7dc8d81b419879fd4be12f056", // new
 		}
-		got, err := calculateNewReleaseModules(filepath.Join("../../test/golden/newupdatedandunchanged-release", bufstate.SyncRoot), prevRelease, currentRelease)
+		got, err := calculateNewReleaseModules(filepath.Join("testdata/golden/newupdatedandunchanged-release", bufstate.SyncRoot), prevRelease, currentRelease)
 		require.NoError(t, err)
 		require.Equal(t, map[string]releaseModuleState{
 			"envoyproxy/protoc-gen-validate": {
@@ -113,7 +116,7 @@ func Test_calculateNewReleaseModules(t *testing.T) {
 	})
 }
 
-func Test_mapGlobalStateReferences(t *testing.T) {
+func TestMapGlobalStateReferences(t *testing.T) {
 	t.Parallel()
 	t.Run("nil_state", func(t *testing.T) {
 		var globalState *modulestatev1alpha1.GlobalState
@@ -140,9 +143,10 @@ func Test_mapGlobalStateReferences(t *testing.T) {
 	})
 }
 
-func Test_createReleaseBody(t *testing.T) {
+func TestCreateReleaseBody(t *testing.T) {
 	t.Parallel()
 	t.Run("Basic", func(t *testing.T) {
+		t.Parallel()
 		mods := map[string]releaseModuleState{
 			"test-org/test-repo": {
 				status: modules.New,
@@ -158,14 +162,13 @@ func Test_createReleaseBody(t *testing.T) {
 			},
 		}
 
-		want := `# Buf Modules Release 20230519.1
+		const want = `# Buf Modules Release 20230519.1
 
 ## New Modules
-<details>
-	<summary>test-org/test-repo: 2 update(s)</summary>
+<details><summary>test-org/test-repo: 2 update(s)</summary>
 
 | Reference | Manifest Digest |
-|---------|--------|
+|---|---|
 | v1.0.0 | fakedigest |
 | v1.1.0 | fakedigest |
 
@@ -177,6 +180,7 @@ func Test_createReleaseBody(t *testing.T) {
 		require.Equal(t, want, got)
 	})
 	t.Run("NewAndUpdated", func(t *testing.T) {
+		t.Parallel()
 		mods := map[string]releaseModuleState{
 			"test-org/new-repo": {
 				status: modules.New,
@@ -204,25 +208,23 @@ func Test_createReleaseBody(t *testing.T) {
 			},
 		}
 
-		want := `# Buf Modules Release 20230519.1
+		const want = `# Buf Modules Release 20230519.1
 
 ## New Modules
-<details>
-	<summary>test-org/new-repo: 2 update(s)</summary>
+<details><summary>test-org/new-repo: 2 update(s)</summary>
 
 | Reference | Manifest Digest |
-|---------|--------|
+|---|---|
 | v1.0.0 | fakedigest |
 | v1.1.0 | fakedigest |
 
 </details>
 
 ## Updated Modules
-<details>
-	<summary>test-org/updated-repo: 2 update(s)</summary>
+<details><summary>test-org/updated-repo: 2 update(s)</summary>
 
 | Reference | Manifest Digest |
-|---------|--------|
+|---|---|
 | v1.0.0 | fakedigest |
 | v1.1.0 | fakedigest |
 
@@ -234,6 +236,7 @@ func Test_createReleaseBody(t *testing.T) {
 		require.Equal(t, want, got)
 	})
 	t.Run("NewUpdatedUnchanged", func(t *testing.T) {
+		t.Parallel()
 		mods := map[string]releaseModuleState{
 			"test-org/new-repo": {
 				status: modules.New,
@@ -264,31 +267,28 @@ func Test_createReleaseBody(t *testing.T) {
 			},
 		}
 
-		want := `# Buf Modules Release 20230519.1
+		const want = `# Buf Modules Release 20230519.1
 
 ## New Modules
-<details>
-	<summary>test-org/new-repo: 1 update(s)</summary>
+<details><summary>test-org/new-repo: 1 update(s)</summary>
 
 | Reference | Manifest Digest |
-|---------|--------|
+|---|---|
 | v1.0.0 | fakedigest |
 
 </details>
 
 ## Updated Modules
-<details>
-	<summary>test-org/updated-repo: 1 update(s)</summary>
+<details><summary>test-org/updated-repo: 1 update(s)</summary>
 
 | Reference | Manifest Digest |
-|---------|--------|
+|---|---|
 | v1.0.0 | fakedigest |
 
 </details>
 
 ## Unchanged Modules
-<details>
-	<summary>Expand</summary>
+<details><summary>Expand</summary>
 
 - test-org/unchanged-repo
 
@@ -297,5 +297,92 @@ func Test_createReleaseBody(t *testing.T) {
 		got, err := createReleaseBody("20230519.1", mods)
 		require.NoError(t, err)
 		require.Equal(t, want, got)
+	})
+}
+
+func TestWriteReferencesTable(t *testing.T) {
+	populateReferences := func(refsCount int) []*modulestatev1alpha1.ModuleReference {
+		refs := make([]*modulestatev1alpha1.ModuleReference, refsCount)
+		for i := 0; i < refsCount; i++ {
+			refs[i] = &modulestatev1alpha1.ModuleReference{
+				Name:   fmt.Sprintf("commit%03d", i),
+				Digest: fmt.Sprintf("digest%03d", i),
+			}
+		}
+		return refs
+	}
+	t.Run("lessThanLimit", func(t *testing.T) {
+		const (
+			moduleName = "foo/bar"
+			refsCount  = 5
+		)
+		var sb strings.Builder
+		require.NoError(t, writeUpdatedReferencesTable(&sb, moduleName, populateReferences(refsCount)))
+		const want = `<details><summary>foo/bar: 5 update(s)</summary>
+
+| Reference | Manifest Digest |
+|---|---|
+| commit000 | digest000 |
+| commit001 | digest001 |
+| commit002 | digest002 |
+| commit003 | digest003 |
+| commit004 | digest004 |
+
+</details>
+`
+		assert.Equal(t, want, sb.String())
+	})
+	t.Run("rightInTheLimit", func(t *testing.T) {
+		const (
+			moduleName = "foo/bar"
+			refsCount  = 10
+		)
+		var sb strings.Builder
+		require.NoError(t, writeUpdatedReferencesTable(&sb, moduleName, populateReferences(refsCount)))
+		const want = `<details><summary>foo/bar: 10 update(s)</summary>
+
+| Reference | Manifest Digest |
+|---|---|
+| commit000 | digest000 |
+| commit001 | digest001 |
+| commit002 | digest002 |
+| commit003 | digest003 |
+| commit004 | digest004 |
+| commit005 | digest005 |
+| commit006 | digest006 |
+| commit007 | digest007 |
+| commit008 | digest008 |
+| commit009 | digest009 |
+
+</details>
+`
+		assert.Equal(t, want, sb.String())
+	})
+	t.Run("afterLimit", func(t *testing.T) {
+		const (
+			moduleName = "foo/bar"
+			refsCount  = 100
+		)
+		var sb strings.Builder
+		require.NoError(t, writeUpdatedReferencesTable(&sb, moduleName, populateReferences(refsCount)))
+		const want = `<details><summary>foo/bar: 100 update(s)</summary>
+
+| Reference | Manifest Digest |
+|---|---|
+| commit000 | digest000 |
+| commit001 | digest001 |
+| commit002 | digest002 |
+| commit003 | digest003 |
+| commit004 | digest004 |
+| ... 90 references skipped ... | ... 90 references skipped ... |
+| commit095 | digest095 |
+| commit096 | digest096 |
+| commit097 | digest097 |
+| commit098 | digest098 |
+| commit099 | digest099 |
+
+</details>
+`
+		assert.Equal(t, want, sb.String())
 	})
 }
