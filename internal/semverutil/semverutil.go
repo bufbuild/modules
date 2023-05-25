2407fee8b96b616332de2839ca13766d62a2b2cd
Julian Figueroa <jfigueroa@buf.build>
2023-05-25 10:13:07 -0500

    Fix lint

--- internal/semverutil/semverutil.go
+++ internal/semverutil/semverutil.go
@@ -67,7 +67,7 @@ func SemverTagNamesAtLeast(semverTagNames []string, minimumSemverTagName string,
 
 // SemverTagNamesExcept gets the semver tag names minus the skip tag names.
 func SemverTagNamesExcept(semverTagNames []string, skipTagNames map[string]struct{}) []string {
-	if skipTagNames == nil || len(skipTagNames) == 0 {
+	if len(skipTagNames) == 0 {
 		// nothing to skip
 		return semverTagNames
 	}
