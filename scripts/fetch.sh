178761c538b2006ee029f5edd721ba142e0ffc54
Philip K. Warren <pwarren@buf.build>
2023-07-20 15:35:25 -0500

    Add bufbuild/confluent managed module
    Fixes #197.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -175,6 +175,7 @@ trap cleanup EXIT
 
 # Keep this module list synced with README.md
 # sync_references ${sync_strategy} ${owner} ${repo} ${git_remote} ${opt_proto_subdir}
+sync_references commits bufbuild confluent https://github.com/bufbuild/confluent-proto
 sync_references releases bufbuild protovalidate https://github.com/bufbuild/protovalidate proto/protovalidate
 sync_references releases bufbuild protovalidate-testing https://github.com/bufbuild/protovalidate proto/protovalidate-testing
 sync_references commits cncf xds https://github.com/cncf/xds
