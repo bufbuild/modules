ba6f88fb1bc39bc75221c0a6b0319acc5178d792
Philip K. Warren <pkwarren@users.noreply.github.com>
2026-08-21 11:34:39 -0500

    Update WKT protos to include newer files (#1301)
    protoc v36.0 ships new .proto files with its binary distributions,
    including some files we missed from previous releases. Update the synced
    files to include the 5 new .proto files.
    
    Rewind the release from earlier today so a new sync can pick up all of
    the files and ship it with the v36.0 label.

--- modules/static/protocolbuffers/wellknowntypes/pre-sync.sh
+++ modules/static/protocolbuffers/wellknowntypes/pre-sync.sh
@@ -1,2 +1,3 @@
 cp ../java/core/src/main/resources/google/protobuf/java_features.proto google/protobuf
 cp ../go/google/protobuf/go_features.proto google/protobuf
+cp ../csharp/google/protobuf/c_sharp_features.proto google/protobuf
